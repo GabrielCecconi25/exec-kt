ac7fb9a8d96cb8edf217ac66aa5c3f7328aa2bd7
Gabriel T. Cecconi <gabriel.cecconi25@gmail.com>
2025-02-20 15:57:11 -0300

    ex4

--- src/module09/ex4.kt
+++ src/module09/ex4.kt
@@ -1,20 +1,29 @@
 package module09
 
 fun main() {
-    gerador4("Olá, Mundo!", 4, 1)
+    gerador4("Olá, Mundo!", 4, 4)
 }
 
 fun gerador4(linha: String, r: Int, b: Int) {
+    var outline: String
 
+    outline = geradorLinha(linha, b)
 
-    val outline: String
-    if (linha.length%2 == 1) {
-        outline = "${"-+".repeat(linha.length/2*2)}-"
-    } else {
-        outline = "-+".repeat(linha.length/2*2)
-    }
-    val message= "${" ".repeat(linha.length/2)}$linha${" ".repeat(linha.length/2)}"
+    val message = "${" ".repeat(linha.length/2)}$linha${" ".repeat(linha.length/2)}"
     println(outline)
     print("${"$message\n".repeat(r)}")
     println(outline)
+}
+
+fun geradorLinha(linha: String, b: Int): String {
+    var outline = ""
+    when (b) {
+        1 -> outline =  "${"-+".repeat(linha.length/2)}${"=".repeat(linha.length)}${"+-".repeat(linha.length/2)}"
+        2 -> outline = "${"~".repeat(linha.length/2-1)}${":".repeat(linha.length+2)}${"~".repeat(linha.length/2-1)}"
+        3 -> outline = "+${"-".repeat(linha.length/2-2)}${"=".repeat(linha.length+2)}${"-".repeat(linha.length/2-2)}+"
+        else -> print("Só temos 3 tipos de linhas, escolher de 1 a 3")
+    }
+
+    return outline
+
 }
