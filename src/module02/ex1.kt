1078d07d48f83dcc304674e8795574c31332d0f9
Alcenir Gomes da Costa <126527994+AlcenirGomes@users.noreply.github.com>
2025-02-25 12:04:28 -0300

    Update ex1.kt
    Exercício 01 do Modulo 02

--- src/module02/ex1.kt
+++ src/module02/ex1.kt
@@ -1,5 +1,20 @@
-package module02
+package modulo02
 
-fun main() {
-    println("Olá, Mundo!")
+/**
+ * Escreva um programa que pergunte a velocidade de um carro.
+ * Caso ultrapasse 80Km/h, exiba uma mensagem dizendo que o usuário
+ * foi multado. Nesse caso, exiba o valor da multa, cobrando R$5 por
+ * cada Km acima da velocidade permitida.
+ */
+
+fun main(){
+
+    println("Digite a velocidade do carro: ")
+    val velocidade = readln().toInt()
+    val velocidadePermitida = 80
+
+    if (velocidade > velocidadePermitida){
+        val multa = (velocidade - velocidadePermitida) * 5
+        println("O carro foi multado. O valor da multa é de R$$multa")
+    }
 }
